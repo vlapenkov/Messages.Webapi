1b54c4c4587d9089322c1cca195848f6938601a8
LapenkovVI <vlapenkov@ya.ru>
2022-10-10 17:15:53 +0300

    +editorconfig

--- Messages.Webapi/StartupExtensions.cs
+++ Messages.Webapi/StartupExtensions.cs
@@ -1,4 +1,4 @@
-﻿using FluentValidation;
+using FluentValidation;
 using Hellang.Middleware.ProblemDetails;
 using Messages.Common;
 using Microsoft.AspNetCore.Http;
@@ -10,62 +10,57 @@ using System.Linq;
 
 namespace Messages.Spa
 {
-        public static class StartupExtensions
+    public static class StartupExtensions
+    {
+        /// <summary>
+        /// Обработка ошибок рефит на фронте
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="env"></param>
+        public static void AddErrorHandling(this IServiceCollection services, IHostEnvironment env)
         {
-                /// <summary>
-                /// Обработка ошибок рефит на фронте
-                /// </summary>
-                /// <param name="services"></param>
-                /// <param name="env"></param>
-                public static void AddErrorHandling(this IServiceCollection services, IHostEnvironment env) {
 
-                        services.AddProblemDetails(
-                options =>
+            services.AddProblemDetails(options =>
                 {
-                        options.IncludeExceptionDetails = (ctx, ex) => !env.IsDevelopment( );
+                    options.IncludeExceptionDetails = (ctx, ex) => !env.IsDevelopment();
 
-                        options.Map<ValidationException>(
+                    options.Map<ValidationException>(
 
-                               delegate (ValidationException exception)
+                           delegate (ValidationException exception)
+                           {
+                               var result = new ValidationProblemDetails(exception.Errors.GroupBy(x => x.PropertyName).ToDictionary(
+                                x => x.Key, x => x.Select(x => x.ErrorMessage).ToArray()))
                                {
+                                   Title = "Ошибка",
+                                   Type = nameof(ValidationException),
+                                   Status = StatusCodes.Status400BadRequest
+                               };
 
-                                       var result = new ValidationProblemDetails( exception.Errors
-                    .GroupBy( x => x.PropertyName )
-                    .ToDictionary(
-                        x => x.Key,
-                        x => x.Select( x => x.ErrorMessage )
-                        .ToArray( ) ) )
-                                       {
-                                               Title = "Ошибка",
-                                               Type = nameof( ValidationException ),
-                                               Status = StatusCodes.Status400BadRequest
-                                       };
+                               return result;
+                           }
+                    );
 
-                                       return result;
-                               }
-                        );
+                    options.Map<RkErrorException>(exception => new ProblemDetails
+                    {
+                        Type = nameof(RkErrorException),
+                        Title = "Ошибка",
+                        Detail = exception.Message,
+                        Status = StatusCodes.Status500InternalServerError
+                    });
 
-                        options.Map<RkErrorException>( exception => new ProblemDetails
-                        {
-                                Type = nameof( RkErrorException ),
-                                Title = "Ошибка",
-                                Detail = exception.Message,
-                                Status = StatusCodes.Status500InternalServerError
-                        } );
-
-                        options.Map<Exception>( exception => new ProblemDetails
-                        {
-                                Type = "TneErrorException",
-                                Title = "Ошибка",
-                                Detail = exception.Message,
-                                Status = StatusCodes.Status500InternalServerError
-                        } );
+                    options.Map<Exception>(exception => new ProblemDetails
+                    {
+                        Type = "TneErrorException",
+                        Title = "Ошибка",
+                        Detail = exception.Message,
+                        Status = StatusCodes.Status500InternalServerError
+                    });
 
 
                 }
 
                 );
-                }
-
         }
+
+    }
 }

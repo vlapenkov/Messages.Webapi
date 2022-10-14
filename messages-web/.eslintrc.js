5d41a42a304a2d5e4fe14e6ec77c7ef4062f0d9c
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-10-14 16:49:38 +0300

    prettier

--- messages-web/.eslintrc.js
+++ messages-web/.eslintrc.js
@@ -5,8 +5,10 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
+    'plugin:import/recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
+    'prettier',
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -14,16 +16,41 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/component-api-style': ['error', ['script-setup', 'composition']],
+    'vue/component-name-in-template-casing': [
+      'error',
+      'kebab-case',
+      { registeredComponentsOnly: false },
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        caughtErrorsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'error',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.ts', '.vue'],
+      },
+    },
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
     },
   ],
 };
+

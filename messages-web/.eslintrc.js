4be5b9813bb680f13f5a57c40d850d382b5ce71a
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-10-17 14:55:18 +0300

    Настройки линта

--- messages-web/.eslintrc.js
+++ messages-web/.eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console':
+      process.env.NODE_ENV === 'production' ? ['error', { allow: ['warn', 'error'] }] : 'warn',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/component-api-style': ['error', ['script-setup', 'composition']],
     'vue/component-name-in-template-casing': [
--- messages-web/vue.config.js
+++ messages-web/vue.config.js
@@ -2,4 +2,13 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    client: {
+      overlay: {
+        warnings: false,
+        errors: true,
+      },
+    },
+  },
 });
+

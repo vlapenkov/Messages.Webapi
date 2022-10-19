2490e624b3fb70e68e967471fe4f2c0c2622ae9b
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-10-19 21:29:19 +0300

    Дополнение к предыдущему

--- messages-web/src/app/core/cqrs/base/@types/ICommand.ts
+++ messages-web/src/app/core/cqrs/base/@types/ICommand.ts
@@ -1,7 +1,6 @@
 export const commandMarker = Symbol('--command-marker');
 
 /** Маркерный интерфейс для команд */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export interface ICommand<T> {
-  [commandMarker]: never;
+  [commandMarker]: never & T;
 }
--- messages-web/src/app/core/cqrs/base/@types/IQuery.ts
+++ messages-web/src/app/core/cqrs/base/@types/IQuery.ts
@@ -1,7 +1,6 @@
 export const queryMarker = Symbol('--query-marker');
 
 /** Маркерный интерфейс для запросов */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export interface IQuery<T> {
-  [queryMarker]: never;
+  [queryMarker]: never & T;
 }

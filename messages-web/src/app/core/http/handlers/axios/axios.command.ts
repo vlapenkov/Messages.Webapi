01b5c7aeea1df22ba5654334739a292690e32bff
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-10-19 20:32:33 +0300

    Чистка

--- messages-web/src/app/core/http/handlers/axios/axios.command.ts
+++ messages-web/src/app/core/http/handlers/axios/axios.command.ts
@@ -1,5 +1,4 @@
 import { ICommand } from '@/app/core/cqrs/base/@types/ICommand';
 import { AxiosHandler } from './axios.handler';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export class AxiosCommand<TOut, Tin extends ICommand<TOut>> extends AxiosHandler<Tin, TOut> {}
--- messages-web/src/app/core/http/handlers/axios/axios.handler.ts
+++ messages-web/src/app/core/http/handlers/axios/axios.handler.ts
@@ -3,7 +3,6 @@ import { AxiosPromise } from 'axios';
 import { http } from '../../http.service';
 import { AxiosHandlerFunction } from './@types/AxiosHandlerFunction';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export class AxiosHandler<Tin, TOut> extends HandlerBase<Tin, AxiosPromise<TOut>> {
   constructor(private fn: AxiosHandlerFunction<Tin, TOut>) {
     super();

be9d615d6faeb15b47ef8d92c3a04f6fb414a5e0
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-10-19 19:57:22 +0300

    unwrap decorator

--- messages-web/src/app/core/services/http/core/decorators/axios-promise-unwrap.decorator.ts
+++ messages-web/src/app/core/services/http/core/decorators/axios-promise-unwrap.decorator.ts
@@ -1,13 +1,19 @@
-import { AxiosPromise } from 'axios';
+import { AxiosPromise, HttpStatusCode } from 'axios';
 import { HandlerDecoratorBase } from '../../../cqrs/base/HandlerDecoratorBase';
+import { ErrorResult } from '../results/error.result';
+import { HttpResult } from '../results/http-result';
+import { Ok } from '../results/ok.result';
 
 export class AxiosPromiseUnwrapDecorator<Tin, TOut> extends HandlerDecoratorBase<
   Tin,
-  Promise<TOut>,
+  Promise<HttpResult<TOut>>,
   AxiosPromise<TOut>
 > {
-  async handle(input: Tin): Promise<TOut> {
-    this.decorated.handle(input);
-    throw new Error('Method not implemented.');
+  async handle(input: Tin): Promise<HttpResult<TOut>> {
+    const response = await this.decorated.handle(input);
+    if (response.status === HttpStatusCode.Ok) {
+      return new Ok(response.data);
+    }
+    return new ErrorResult<TOut>(response.statusText);
   }
 }

4733376aaf85faf1a4185e6a3ff6d5f0d84e6bc0
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-10-26 21:31:38 +0300

    Тестовый парсинг

--- messages-web/src/app/sections/services/infrastructure/sections.service.ts
+++ messages-web/src/app/sections/services/infrastructure/sections.service.ts
@@ -1,10 +1,16 @@
+import { extend } from '@/app/core/handlers/base/handler-lab';
 import { defineHttpService } from '@/app/core/services/http/define-http.service';
-import { ISectionModel } from '../../models/section.model';
+import { parseArray } from '@/app/core/services/http/handlers/parse.handlers';
+import { ISectionModel, SectionModel } from '../../models/section.model';
 
 export const { defineGet } = defineHttpService<ISectionModel>({
   url: 'sections',
 });
 
-export const getAll = defineGet(() => ({
-  url: '/list',
-}));
+export const getAll = extend(
+  defineGet(() => ({
+    url: '/list',
+  })),
+)
+  .pipe(parseArray(SectionModel))
+  .done();

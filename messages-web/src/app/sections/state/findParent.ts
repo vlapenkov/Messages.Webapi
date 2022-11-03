d6381d278015c6aab65331185c7372bd627acdc1
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-11-03 16:23:15 +0300

    Чистка

--- messages-web/src/app/sections/state/findParent.ts
+++
@@ -1,8 +0,0 @@
-import { SectionModel } from '../models/section.model';
-import { sectionsStore } from './sections.store';
-
-export function findParent(m: SectionModel) {
-  return m.parentSectionId != null
-    ? sectionsStore.items.value?.find((i) => i.id === m.parentSectionId)?.name ?? 'Не найден'
-    : 'Отсутcтвует';
-}

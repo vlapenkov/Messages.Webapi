9644f8476f95a1a3960c34c9786eb65e5d4cb44b
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-11-08 21:12:43 +0300

    Делаем кнопки outlined на тёмном фоне

--- messages-web/src/vue/base/components/prime/buttons/base/PrimeButton.vue
+++ messages-web/src/vue/base/components/prime/buttons/base/PrimeButton.vue
@@ -38,9 +38,10 @@ export default defineComponent({
     },
   },
   setup(props, { slots, attrs }) {
-    const buttonStyle = computed(() =>
-      isDark.value ? `${defaultButtonStyle} p-button-outlined` : defaultButtonStyle,
-    );
+    const buttonStyle = computed(() => ({
+      [defaultButtonStyle]: true,
+      ' p-button-outlined': isDark.value,
+    }));
     return () =>
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       h(Button as any, { ...props, class: attrs.class ?? buttonStyle.value }, { ...slots });

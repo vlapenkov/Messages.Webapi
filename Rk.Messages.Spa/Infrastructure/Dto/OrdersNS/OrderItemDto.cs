44c71cd9a7072b71f97f37a2c5936a01f26b256e
Lapenkov.VI@roscosmos.digital <Lapenkov.VI@roscosmos.digital>
2022-11-09 12:06:17 +0300

    Список заказов с фильтром

--- Rk.Messages.Spa/Infrastructure/Dto/OrdersNS/OrderItemDto.cs
+++ Rk.Messages.Spa/Infrastructure/Dto/OrdersNS/OrderItemDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rk.Messages.Spa.Infrastructure.Dto.OrdersNS
+﻿namespace Rk.Messages.Spa.Infrastructure.Dto.OrdersNS
 {
     public record OrderItemDto
     {
--- Rk.Messages.Spa/Infrastructure/Services/IOrdersService.cs
+++ Rk.Messages.Spa/Infrastructure/Services/IOrdersService.cs
@@ -1,16 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Refit;
+using Rk.Messages.Spa.Infrastructure.Dto.CommonNS;
 using Rk.Messages.Spa.Infrastructure.Dto.OrdersNS;
 
 namespace Rk.Messages.Spa.Infrastructure.Services
 {
     public interface IOrdersService
     {
+        /// <summary>
+        /// Создать заказ
+        /// </summary>
+        /// <returns>Номер заказа</returns>
         [Post("/api/v1/Orders")]
         Task<long> CreateOrder();
 
+        /// <summary>
+        /// Получить заказ
+        /// </summary>
+        /// <param name="orderId">Id заказа</param>
+        /// <returns></returns>
         [Get("/api/v1/Orders/{orderId}")]
         Task<OrderResponse> GetOrder(long orderId);
-        
+
+        /// <summary>
+        /// Получить пагинированный список заказов
+        /// </summary>
+        /// <param name="request">фильтр</param>
+        /// <returns>пагинированный список заказов</returns>
+        [Get("/api/v1/Orders")]
+        Task<PagedResponse<OrderShortDto>> GetOrders([Query] FilterOrdersRequest request);
     }
 }

88fb52fa123f1bab76d1133c1162e86b172031e9
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-11-14 17:58:12 +0300

    Пробрасываем выше

--- messages-web/src/vue/base/presentational/state/collection/components/data-views/tree-view-collection.vue
+++ messages-web/src/vue/base/presentational/state/collection/components/data-views/tree-view-collection.vue
@@ -1,25 +1,8 @@
 <template>
   <loading-status-handler>
     <tree class="mt-2 label-full" :value="tree">
-      <template #default="{ node }">
-        <model-provider :data="node.data">
-          <hover-tag
-            #default="{ hover }"
-            style="min-height: 50px"
-            class="flex w-full border-round justify-content-between p-2 flex-row align-items-center gap-3 tree-node-hover"
-          >
-            <span>{{ node.label }}</span>
-            <transition
-              mode="out-in"
-              enter-active-class="fadeinleft animation-duration-100"
-              leave-active-class="fadeoutleft animation-duration-100"
-            >
-              <div v-if="hover" class="flex flex-row gap-2">
-                <edit-item-button class="icon-small" />
-              </div>
-            </transition>
-          </hover-tag>
-        </model-provider>
+      <template v-for="slotName in slotNames" #[slotName]="{ node }" :key="slotName">
+        <slot :name="slotName" :node="node"></slot>
       </template>
     </tree>
   </loading-status-handler>
@@ -30,37 +13,19 @@ import { defineComponent } from 'vue';
 import { useTree } from '../../composables/tree.composable';
 
 export default defineComponent({
-  setup() {
-    return { tree: useTree() };
+  setup(_, { slots }) {
+    return { tree: useTree(), slotNames: Object.keys(slots) };
   },
 });
 </script>
 
 <style lang="scss" scoped>
-$size: 0;
-$padding-and-font: 1rem;
-.icon-small {
-  height: $size !important;
-  width: $size;
-  border-radius: $padding-and-font * 2;
-  padding: $padding-and-font;
-  :deep(.p-button-icon) {
-    font-size: $padding-and-font;
-  }
-}
 .label-full {
   :deep(.p-treenode-label) {
     width: 100%;
   }
   :deep(.p-treenode-content) {
     padding: 0.25rem 0 !important;
-  }
-}
-
-.tree-node-hover {
-  transition: background-color 0.2s ease-in-out;
-  &:hover {
-    background-color: var(--surface-ground);
   }
 }
 </style>
--- messages-web/src/vue/views/sections-view.vue
+++ messages-web/src/vue/views/sections-view.vue
@@ -14,7 +14,28 @@
       <template #tree-view>
         <splitter class="mt-2">
           <splitter-panel :size="30">
-            <tree-view-collection class="reshape-tree"></tree-view-collection>
+            <tree-view-collection class="reshape-tree">
+              <template #default="{ node }">
+                <model-provider :data="node.data">
+                  <hover-tag
+                    #default="{ hover }"
+                    style="min-height: 50px"
+                    class="flex w-full border-round justify-content-between p-2 flex-row align-items-center gap-3 tree-node-hover"
+                  >
+                    <span>{{ node.label }}</span>
+                    <transition
+                      mode="out-in"
+                      enter-active-class="fadeinleft animation-duration-100"
+                      leave-active-class="fadeoutleft animation-duration-100"
+                    >
+                      <div v-if="hover" class="flex flex-row gap-2">
+                        <edit-item-button class="icon-small" />
+                      </div>
+                    </transition>
+                  </hover-tag>
+                </model-provider>
+              </template>
+            </tree-view-collection>
           </splitter-panel>
           <splitter-panel :size="70">
             <router-view></router-view>
@@ -44,6 +65,25 @@ export default defineComponent({
   :deep(.p-tree) {
     margin-top: 0 !important;
     border: none;
+  }
+}
+
+$size: 0;
+$padding-and-font: 1rem;
+.icon-small {
+  height: $size !important;
+  width: $size;
+  border-radius: $padding-and-font * 2;
+  padding: $padding-and-font;
+  :deep(.p-button-icon) {
+    font-size: $padding-and-font;
+  }
+}
+
+.tree-node-hover {
+  transition: background-color 0.2s ease-in-out;
+  &:hover {
+    background-color: var(--surface-ground);
   }
 }
 </style>

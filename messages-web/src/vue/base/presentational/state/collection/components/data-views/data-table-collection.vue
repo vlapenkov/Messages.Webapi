a45aeffdb5a2909b6378a853742ecffe4c0c46dc
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-11-14 20:35:47 +0300

    data-table draft

--- messages-web/src/vue/base/presentational/state/collection/components/data-views/data-table-collection.vue
+++ messages-web/src/vue/base/presentational/state/collection/components/data-views/data-table-collection.vue
@@ -1,13 +1,15 @@
 <template>
-  <div></div>
+  <data-table :value="items"></data-table>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useItems } from '../../composables/items.composable';
 
 export default defineComponent({
   setup() {
-    return {};
+    const items = useItems();
+    return { items };
   },
 });
 </script>

39e75955ab95465de8addda5817529d2bf56f252
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-11-15 14:16:36 +0300

    Поделил файлы

--- messages-web/src/app/orders/state/orders.store.ts
+++ messages-web/src/app/orders/state/orders.store.ts
@@ -1,27 +1,7 @@
 import { definePageableCollectionStore } from '@/app/core/services/harlem/custom-stores/pageable-collection/pageable-collection.store';
-import { StateBase } from '@/app/core/services/harlem/state/base/state-base';
-import { dataStatus } from '@/app/core/services/harlem/state/decorators/data-status.decorator';
-import { pageNumber } from '@/app/core/services/harlem/state/decorators/page-number.decorator';
-import { pageSize } from '@/app/core/services/harlem/state/decorators/page-size-decorator';
-import { pages } from '@/app/core/services/harlem/state/decorators/pages.decorator';
-import { DataStatus } from '@/app/core/services/harlem/tools/data-status';
-import { IPagedResponse } from '@/app/core/services/http/@types/IPagedResponse';
 import { ordersHttpService } from '../infrastructure/order.http-service';
 import { OrderModel } from '../model/order.model';
-
-export class OrdersState extends StateBase {
-  @pages
-  pages: IPagedResponse<OrderModel>[] = [];
-
-  @dataStatus
-  status = new DataStatus();
-
-  @pageNumber
-  pageNumber = 1;
-
-  @pageSize
-  pageSize = 12;
-}
+import { OrdersState } from './orders.state';
 
 export const ordersStore = definePageableCollectionStore(
   'orders',

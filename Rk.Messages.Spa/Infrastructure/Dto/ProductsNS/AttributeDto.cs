2084508f8e8147294e041cc54fc97f028f370091
Lapenkov.VI@roscosmos.digital <Lapenkov.VI@roscosmos.digital>
2022-11-21 09:31:13 +0300

    Endpoint для атрибутов

---
+++ Rk.Messages.Spa/Infrastructure/Dto/ProductsNS/AttributeDto.cs
@@ -0,0 +1,11 @@
+﻿using Rk.Messages.Spa.Infrastructure.Dto.CommonNS;
+
+namespace Rk.Messages.Spa.Infrastructure.Dto.ProductsNS
+{
+    /// <summary>
+    /// Атрибут продукции
+    /// </summary>
+    public record AttributeDto : BaseDto
+    {
+    }
+}

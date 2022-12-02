c4dc0b6b32b020831f9f9ff69d9b125f62164ebd
Гиленко Андрей <gilenko.aa@roscosmos.digital>
2022-12-02 09:47:56 +0300

    переход к организации

--- messages-web/src/vue/containers/organizations/popular-organizations-list.vue
+++ messages-web/src/vue/containers/organizations/popular-organizations-list.vue
@@ -2,7 +2,7 @@
   <div class="popular-organization-list">
     <div class="grid w-full">
       <div v-for="o in items" :key="o.id" class="col-3">
-        <card class="h-full p-2">
+        <card class="h-full p-2" @click="viewOrganization(o)" :style="{ cursor: 'pointer' }">
           <template #content>
             <div class="h-full w-full flex flex-row justify-space-between align-items-center">
               <div class="h-full w-full grid">
@@ -34,17 +34,23 @@
 import { OrganizationModel } from '@/app/organizations/model/organization.model';
 import { organizationsStore } from '@/app/organizations/state/organizations.store';
 import { computed, defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
   setup() {
+    const router = useRouter();
     const items = computed(() => {
       const data = [...(organizationsStore.currentPageItems.value ?? [])];
       return data.sort((a: OrganizationModel, b: OrganizationModel) =>
         a.name[0].localeCompare(b.name[0]),
       );
     });
+    const viewOrganization = (item: OrganizationModel) => {
+      router.push({ name: 'organization', params: { id: item.id } });
+    };
     return {
       items,
+      viewOrganization,
     };
   },
 });

8e96f06e29d3e5a48bef301f2653f43ec1477aff
Гиленко Андрей <gilenko.aa@roscosmos.digital>
2022-12-12 17:52:52 +0300

    переверстал форму

--- messages-web/src/vue/views/organization-add-view.vue
+++ messages-web/src/vue/views/organization-add-view.vue
@@ -1,41 +1,14 @@
 <!-- eslint-disable vuejs-accessibility/label-has-for -->
 <template>
-  <app-page title="Создание организации">
+  <app-page title="Профиль организации">
     <div class="flex flex-column">
       <card class="add-organization-inner-card">
         <template #content>
-          <div class="w-full h-full grid">
-            <div class="col-6">
-              <h3 class="mt-0">Сведения о предприятии</h3>
-              <div class="field">
-                <label for="ogrn">ОГРН</label>
-                <input-text
-                  id="ogrn"
-                  type="text"
-                  class="w-full p-inputtext-sm"
-                  v-model="formState.ogrn"
-                />
-              </div>
-              <div class="field">
-                <label for="inn">ИНН</label>
-                <input-text
-                  id="inn"
-                  type="text"
-                  class="w-full p-inputtext-sm"
-                  v-model="formState.inn"
-                />
-              </div>
-              <div class="field">
-                <label for="kpp">КПП</label>
-                <input-text
-                  id="kpp"
-                  type="text"
-                  class="w-full p-inputtext-sm"
-                  v-model="formState.kpp"
-                />
-              </div>
-              <div class="field">
-                <label for="full-name">Полное наименование</label>
+          <div>
+            <h2 class="mt-0">1. Общие сведения об организации</h2>
+            <div class="w-full h-full grid">
+              <div class="col-4 field">
+                <label for="full-name" class="text-600">Полное наименование</label>
                 <input-text
                   id="full-name"
                   type="text"
@@ -43,8 +16,18 @@
                   v-model="formState.fullName"
                 />
               </div>
-              <div class="field">
-                <label for="short-name">Сокращенное наименование</label>
+              <div class="col-4 field">
+                <label for="ogrn" class="text-600">ОГРН</label>
+                <input-text
+                  id="ogrn"
+                  type="text"
+                  class="w-full p-inputtext-sm"
+                  v-model="formState.ogrn"
+                />
+              </div>
+              <div class="col-4"></div>
+              <div class="col-4 field">
+                <label for="short-name" class="text-600">Сокращенное наименование</label>
                 <input-text
                   id="short-name"
                   type="text"
@@ -52,8 +35,18 @@
                   v-model="formState.name"
                 />
               </div>
-              <div class="field">
-                <label for="status">Статус</label>
+              <div class="col-4 field">
+                <label for="kpp" class="text-600">КПП</label>
+                <input-text
+                  id="kpp"
+                  type="text"
+                  class="w-full p-inputtext-sm"
+                  v-model="formState.kpp"
+                />
+              </div>
+              <div class="col-4"></div>
+              <div class="col-4 field">
+                <label for="status" class="text-600">Статус</label>
                 <input-text
                   id="status"
                   type="text"
@@ -61,50 +54,21 @@
                   v-model="formState.statusText"
                 />
               </div>
+              <div class="col-4 field">
+                <label for="inn" class="text-600">ИНН</label>
+                <input-text
+                  id="inn"
+                  type="text"
+                  class="w-full p-inputtext-sm"
+                  v-model="formState.inn"
+                />
+              </div>
+              <div class="col-4"></div>
             </div>
-            <div class="col-6">
-              <h3 class="mt-0">Банковские реквизиты</h3>
-              <div class="field">
-                <label for="bik">БИК</label>
-                <input-text
-                  id="bik"
-                  type="text"
-                  class="w-full p-inputtext-sm"
-                  v-model="formState.bik"
-                />
-              </div>
-              <div class="field">
-                <label for="rc">Р/с</label>
-                <input-text
-                  id="rc"
-                  type="text"
-                  class="w-full p-inputtext-sm"
-                  v-model="formState.rc"
-                />
-              </div>
-              <div class="field">
-                <label for="bank">Банк</label>
-                <input-text
-                  id="bank"
-                  type="text"
-                  class="w-full p-inputtext-sm"
-                  v-model="formState.bank"
-                />
-              </div>
-              <div class="field">
-                <label for="cor-acc">Кор/счёт</label>
-                <input-text
-                  id="cor-acc"
-                  type="text"
-                  class="w-full p-inputtext-sm"
-                  v-model="formState.corAcc"
-                />
-              </div>
-            </div>
-            <div class="col-6">
-              <h3 class="mt-5">Отрасль</h3>
-              <div class="field">
-                <label for="okved">ОКВЭД</label>
+            <h3 class="mt-4">Отрасль</h3>
+            <div class="w-full h-full grid">
+              <div class="col-3 field">
+                <label for="okved" class="text-600">ОКВЭД</label>
                 <input-text
                   id="okved"
                   type="text"
@@ -112,8 +76,8 @@
                   v-model="formState.okved"
                 />
               </div>
-              <div class="field">
-                <label for="okved2">ОКВЭД 2</label>
+              <div class="col-3 field">
+                <label for="okved2" class="text-600">ОКВЭД 2</label>
                 <input-text
                   id="okved2"
                   type="text"
@@ -122,47 +86,122 @@
                 />
               </div>
             </div>
-            <div class="col-6"></div>
-            <div class="col-6">
-              <h3 class="mt-5">Контактные данные</h3>
-              <div class="field">
-                <label for="country">Страна</label>
+          </div>
+          <prime-divider class="mt-5 mb-5"></prime-divider>
+          <div>
+            <h2 class="mt-0">2. Адрес</h2>
+            <div class="w-full h-full grid">
+              <div class="col-8 field">
+                <label for="legal-address" class="text-600">Юридический адрес</label>
                 <input-text
-                  id="country"
+                  id="legal-address"
                   type="text"
                   class="w-full p-inputtext-sm"
-                  v-model="formState.country"
+                  v-model="formState.legalAddress"
+                  disabled
                 />
               </div>
-              <div class="field">
-                <label for="region">Регион</label>
+              <div class="col-3"></div>
+              <div class="col-8 field">
+                <label for="actual-address" class="text-600">Фактический адрес</label>
                 <input-text
-                  id="region"
+                  id="actual-address"
                   type="text"
                   class="w-full p-inputtext-sm"
-                  v-model="formState.region"
+                  v-model="formState.actualAddress"
+                  disabled
                 />
               </div>
-              <div class="field">
-                <label for="city">Город</label>
+              <div class="col-3"></div>
+              <div class="col-8 field">
+                <label for="postal-address" class="text-600">Почтовый адрес</label>
                 <input-text
-                  id="city"
+                  id="postal-address"
                   type="text"
                   class="w-full p-inputtext-sm"
-                  v-model="formState.city"
+                  v-model="formState.postalAddress"
+                  disabled
                 />
               </div>
-              <div class="field">
-                <label for="address">Адрес</label>
+              <div class="col-3"></div>
+            </div>
+          </div>
+          <prime-divider class="mt-5 mb-5"></prime-divider>
+          <div>
+            <h2 class="mt-0">3. Банковские реквизиты</h2>
+            <div class="w-full h-full grid">
+              <div class="col-4 field">
+                <label for="bank" class="text-600">Банк</label>
                 <input-text
-                  id="address"
+                  id="bank"
                   type="text"
                   class="w-full p-inputtext-sm"
-                  v-model="formState.address"
+                  v-model="formState.bank"
+                  disabled
                 />
               </div>
-              <div class="field">
-                <label for="site">Сайт</label>
+              <div class="col-4 field">
+                <label for="cor-acc" class="text-600">Корреспондентский счет</label>
+                <input-text
+                  id="cor-acc"
+                  type="text"
+                  class="w-full p-inputtext-sm"
+                  v-model="formState.corAcc"
+                  disabled
+                />
+              </div>
+              <div class="col-4"></div>
+              <div class="col-4 field">
+                <label for="acc-number" class="text-600">Номер счета</label>
+                <input-text
+                  id="acc-number"
+                  type="text"
+                  class="w-full p-inputtext-sm"
+                  v-model="formState.accNumber"
+                  disabled
+                />
+              </div>
+              <div class="col-4 field">
+                <label for="bik" class="text-600">БИК</label>
+                <input-text
+                  id="bik"
+                  type="text"
+                  class="w-full p-inputtext-sm"
+                  v-model="formState.bik"
+                  disabled
+                />
+              </div>
+              <div class="col-4"></div>
+            </div>
+          </div>
+          <prime-divider class="mt-5 mb-5"></prime-divider>
+          <div>
+            <h2 class="mt-0">4. Контактные данные</h2>
+            <div class="w-full h-full grid">
+              <div class="col-4 field">
+                <label for="phone" class="text-600">Контактный телефон</label>
+                <input-text
+                  id="phone"
+                  type="text"
+                  class="w-full p-inputtext-sm"
+                  v-model="formState.phone"
+                  disabled
+                />
+              </div>
+              <div class="col-8"></div>
+              <div class="col-4 field">
+                <label for="email" class="text-600">E-mail</label>
+                <input-text
+                  id="email"
+                  type="text"
+                  class="w-full p-inputtext-sm"
+                  v-model="formState.email"
+                  disabled
+                />
+              </div>
+              <div class="col-8"></div>
+              <div class="col-4 field">
+                <label for="site" class="text-600">Сайт</label>
                 <input-text
                   id="site"
                   type="text"
@@ -170,20 +209,12 @@
                   v-model="formState.site"
                 />
               </div>
+              <div class="col-8"></div>
             </div>
-            <div class="col-6"></div>
-            <div class="col-6">
-              <div class="mt-5">
-                <div class="field">
-                  <checkbox inputId="buyer" v-model="formState.isBuyer" :binary="true" />
-                  <label for="buyer" class="m-0 ml-1">Покупатель</label>
-                </div>
-                <div class="field">
-                  <checkbox inputId="seller" v-model="formState.isSeller" :binary="true" />
-                  <label for="seller" class="m-0 ml-1">Продавец</label>
-                </div>
-              </div>
-            </div>
+          </div>
+          <prime-divider class="mt-5 mb-5"></prime-divider>
+          <div class="flex flex-row justify-content-end align-items-center">
+            <prime-button label="Сохранить" />
           </div>
         </template>
       </card>
@@ -251,10 +282,7 @@ export default defineComponent({
 .add-organization-inner-card {
   .field {
     display: block;
-    margin-bottom: 1rem;
-    &:last-child {
-      margin-bottom: 0;
-    }
+    margin: 0;
   }
 
   :deep(.p-inputtext) {

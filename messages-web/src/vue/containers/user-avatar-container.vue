cbfb2c0a9e84bb97a1964da0e364419e8f052686
Врублевский Алексей <Vrublevskiy.AA@roscosmos.digital>
2022-12-15 12:16:07 +0300

    -разделил действия пользователя в зависимости от роли

--- messages-web/src/vue/containers/user-avatar-container.vue
+++ messages-web/src/vue/containers/user-avatar-container.vue
@@ -1,12 +1,7 @@
 <!-- eslint-disable vuejs-accessibility/click-events-have-key-events -->
 <template>
-  <div
-    aria-controls="overlay_menu"
-    aria-haspopup="true"
-    @click="toggleMenu"
-    class="flex flex-row align-items-center gap-2 p-1 pl-3 avatar border-round-3xl"
-    v-if="isAuthenticated"
-  >
+  <div aria-controls="overlay_menu" aria-haspopup="true" @click="toggleMenu"
+    class="flex flex-row align-items-center gap-2 p-1 pl-3 avatar border-round-3xl" v-if="isAuthenticated">
     <div>{{ userShortName }}</div>
     <avatar shape="circle" icon="pi pi-user"></avatar>
   </div>
@@ -54,66 +49,91 @@ export default defineComponent({
       return user;
     });
 
-    const menuItems = computed(() => [
-      {
-        label: 'Каталог товаров',
-        to: { name: 'catalog' },
-        icon: 'pi pi-th-large',
-      },
-      {
-        label: 'Мои товары',
-        to: { name: 'products' },
-        icon: 'pi pi-bars',
-      },
-      {
-        label: 'Управление категориями',
-        to: { name: 'categories' },
-        icon: 'pi pi-book',
-      },
-      {
-        label: `Корзина${
-          shoppingCartStore.totalQuantity.value > 0
-            ? ` (${shoppingCartStore.totalQuantity.value})`
-            : ''
-        }`,
-        to: { name: 'shopping-cart' },
-        icon: 'pi pi-shopping-cart',
-        badge: 5,
-      },
-      {
-        label: 'Заказы',
-        to: { name: 'orders' },
-        icon: 'pi pi-box',
-      },
-      {
-        label: 'Отчеты',
-        to: { name: 'reports' },
-        icon: 'pi pi-chart-bar',
-      },
-      {
-        label: 'География производства',
-        to: { name: 'production-geo' },
-        icon: 'pi pi-map',
-      },
-      {
-        label: 'Добавить организацию',
-        to: { name: 'organization-add' },
-        icon: 'pi pi-plus',
-      },
-      {
+    const menuItems = computed(() => {
+      const items = [];
+      const roles = userInfo.value?.role
+      if (roles != null && roles.indexOf("manager_org_seller") >= 0) {
+        items.push(
+          {
+            label: 'Товары',
+            to: { name: 'products' },
+            icon: 'pi pi-bars',
+          },
+          {
+            label: 'Заказы',
+            to: { name: 'orders' },
+            icon: 'pi pi-box',
+          },
+          {
+            label: 'Отчеты',
+            to: { name: 'reports' },
+            icon: 'pi pi-chart-bar',
+          })
+      }
+      if (roles != null && roles.indexOf("manager_org_buyer") >= 0) {
+        items.push({
+          label: 'Каталог товаров',
+          to: { name: 'catalog' },
+          icon: 'pi pi-th-large',
+        },
+          {
+            label: `Корзина${shoppingCartStore.totalQuantity.value > 0
+              ? ` (${shoppingCartStore.totalQuantity.value})`
+              : ''
+              }`,
+            to: { name: 'shopping-cart' },
+            icon: 'pi pi-shopping-cart',
+            badge: 5,
+          },
+          {
+            label: 'Заказы',
+            to: { name: 'orders' },
+            icon: 'pi pi-box',
+          },
+          {
+            label: 'География производства',
+            to: { name: 'production-geo' },
+            icon: 'pi pi-map',
+          })
+      }
+      if (roles != null && roles.indexOf("content_manager") >= 0) {
+        items.push(
+          {
+            label: 'Управление товарами',
+            to: { name: 'products' },
+            icon: 'pi pi-bars',
+          },
+          {
+            label: 'Управление категориями',
+            to: { name: 'categories' },
+            icon: 'pi pi-book',
+          },
+
+          {
+            label: 'Общие отчеты',
+            to: { name: 'reports' },
+            icon: 'pi pi-chart-bar',
+          },
+          {
+            label: 'Управление организациями',
+            to: { name: 'organization-add' },
+            icon: 'pi pi-plus',
+          })
+      }
+      items.push({
         label: 'Выход',
         icon: 'pi pi-sign-out',
         command: () => {
           logout();
         },
-      },
-    ]);
+      })
+      return items;
+    });
 
     const menu = ref();
 
     const toggleMenu = (event: Event) => {
       // console.log('menu', menu.value);
-
       menu.value.toggle(event);
     };
 

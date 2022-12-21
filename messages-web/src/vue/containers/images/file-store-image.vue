3767d9c67b978eced3e058c0c71b93105af454c1
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-12-21 17:02:06 +0300

    Пинаем картинку обновляться

--- messages-web/src/vue/containers/images/file-store-image.vue
+++ messages-web/src/vue/containers/images/file-store-image.vue
@@ -72,6 +72,7 @@ export default defineComponent({
             }
           });
         } else {
+          imageData.value = undefined;
           animationMode.value = 'none';
         }
       },

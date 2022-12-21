38bc16981cde0d440f1f485258fd0409e53e296a
Михайлов Данила <Mikhaylov.DS@roscosmos.digital>
2022-12-21 12:05:07 +0300

    Уменьшил тени

--- messages-web/src/vue/presentational/integrations/markeplaces-tab.vue
+++ messages-web/src/vue/presentational/integrations/markeplaces-tab.vue
@@ -1,14 +1,20 @@
 <template>
-  <card>
+  <card class="shadow-1">
     <template #content>
       <data-table :value="items" responsiveLayout="scroll">
         <column header="Торговая площадка" headerStyle="width: 30%">
           <template #body="slopProps">
             <div class="w-full flex flex-row align-items-center">
-              <img :src="require('@/assets' + slopProps.data.image)" alt="Изображение профиля" width="100" :style="{
-                objectFit: 'cover',
-                borderRadius: '0.5rem',
-              }" class="mr-3" />
+              <img
+                :src="require('@/assets' + slopProps.data.image)"
+                alt="Изображение профиля"
+                width="100"
+                :style="{
+                  objectFit: 'cover',
+                  borderRadius: '0.5rem',
+                }"
+                class="mr-3"
+              />
               <span class="p-component">{{ slopProps.data.name }}</span>
             </div>
           </template>
@@ -31,7 +37,12 @@
         </column>
         <column field="statusText" header="Активность">
           <template #body="slopProps">
-            <checkbox inputId="binary" v-model="slopProps.data.active" :binary="true" :disabled="true" />
+            <checkbox
+              inputId="binary"
+              v-model="slopProps.data.active"
+              :binary="true"
+              :disabled="true"
+            />
           </template>
         </column>
       </data-table>
@@ -52,7 +63,7 @@ export default defineComponent({
         month: 'numeric',
         day: 'numeric',
         hour: 'numeric',
-        minute: 'numeric'
+        minute: 'numeric',
       });
     };
 
@@ -65,10 +76,34 @@ export default defineComponent({
       ordersCount: number;
       active: boolean;
     }[] = [
-        { image: '/images/ozon.svg', name: "Ozon", itemsCount: 234, lastLoaded: new Date('2022-12-13T13:12:00Z'), ordersCount: 43, lastUploaded: new Date('2022-12-13T13:49:00Z'), active: true },
-        { image: '/images/yandex.svg', name: "Яндекс Маркет", itemsCount: 12, lastLoaded: new Date('2022-12-14T12:43:00Z'), ordersCount: 4, lastUploaded: new Date('2022-12-15T13:54:00Z'), active: true },
-        { image: '/images/avito.svg', name: "Avito", itemsCount: 76, lastLoaded: new Date('2022-12-16T15:00:00Z'), ordersCount: 18, lastUploaded: new Date('2022-12-19T14:12:00Z'), active: false }
-      ];
+      {
+        image: '/images/ozon.svg',
+        name: 'Ozon',
+        itemsCount: 234,
+        lastLoaded: new Date('2022-12-13T13:12:00Z'),
+        ordersCount: 43,
+        lastUploaded: new Date('2022-12-13T13:49:00Z'),
+        active: true,
+      },
+      {
+        image: '/images/yandex.svg',
+        name: 'Яндекс Маркет',
+        itemsCount: 12,
+        lastLoaded: new Date('2022-12-14T12:43:00Z'),
+        ordersCount: 4,
+        lastUploaded: new Date('2022-12-15T13:54:00Z'),
+        active: true,
+      },
+      {
+        image: '/images/avito.svg',
+        name: 'Avito',
+        itemsCount: 76,
+        lastLoaded: new Date('2022-12-16T15:00:00Z'),
+        ordersCount: 18,
+        lastUploaded: new Date('2022-12-19T14:12:00Z'),
+        active: false,
+      },
+    ];
     return { items, formatDateString };
   },
 });
@@ -85,7 +120,7 @@ export default defineComponent({
 }
 
 :deep(.p-datatable-thead tr th) {
-  background-color: #FFFFFF;
+  background-color: #ffffff;
 }
 
 :deep(.p-card-content) {

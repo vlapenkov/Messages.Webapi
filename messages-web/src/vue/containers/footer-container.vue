c8216295ca563202c340999d9daa8a948a6088e2
Гиленко Андрей <gilenko.aa@roscosmos.digital>
2022-12-21 10:23:58 +0300

    fix

--- messages-web/src/vue/containers/footer-container.vue
+++ messages-web/src/vue/containers/footer-container.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full bg-gray-100">
     <app-container>
-      <div class="w-full flex flex-column mt-3 mb-4">
+      <div class="w-full flex flex-column mt-4 mb-4">
         <div class="w-full flex flex-row justify-content-between align-items-top">
           <div class="flex flex-column gap-2">
             <span class="p-component text-md text-color font-medium mb-1"> Общая информация </span>
@@ -39,9 +39,17 @@
         </div>
         <prime-divider class="mt-5 mb-3"></prime-divider>
         <div>
-          <span class="p-component text-500 text-sm ml-1">
-            © {{ new Date().getFullYear() }} Маркетплейс продукции гражданского назначения
-          </span>
+          <div class="w-full flex flex-row justify-content-between align-items-center">
+            <span class="p-component text-500 text-sm ml-1">
+              © {{ new Date().getFullYear() }} Маркетплейс продукции гражданского назначения
+            </span>
+            <div>
+              <span class="p-component text-sm text-500"> Разработано </span>
+              <a href="https://roscosmos.digital/" class="no-underline">
+                <span class="p-component text-sm text-primary cursor-pointer"> РК-Цифра </span>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </app-container>

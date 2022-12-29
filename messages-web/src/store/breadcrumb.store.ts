0a188c8ca6dada406690020d93b283e1ce45543d
Гиленко Андрей <gilenko.aa@roscosmos.digital>
2022-12-29 13:53:07 +0300

    почти работает

--- messages-web/src/store/breadcrumb.store.ts
+++ messages-web/src/store/breadcrumb.store.ts
@@ -1,12 +1,18 @@
 import { defineStore } from '@/app/core/services/harlem/harlem.service';
+import { productFullStore } from '@/app/product-full/state/product-full.store';
 import { sectionsStore } from '@/app/sections/state/sections.store';
 import { treeToList } from '@/services/breadcrumb.service';
-import { RouteLocation, RouteLocationRaw } from 'vue-router';
 import { catalogFiltersStore } from './catalog-filters.store';
+
+export interface ILocation {
+  name?: string;
+  path?: string;
+  params?: Record<string | number, unknown>;
+}
 
 export interface ITreeNode {
   label: () => string | undefined;
-  route: RouteLocationRaw | RouteLocation;
+  route: ILocation;
   children?: ITreeNode[];
 }
 
@@ -14,15 +20,20 @@ export interface IListNode {
   id: symbol;
   parentId: symbol | null;
   label: () => string | undefined;
-  route: RouteLocationRaw | RouteLocation;
+  route: ILocation;
 }
 
 export interface IBreadcrumbStore {
   list: IListNode[];
+  params: {
+    sectionId: number | null;
+  };
 }
 
 const { sections } = sectionsStore;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { sectionId } = catalogFiltersStore;
+const { product } = productFullStore;
 const tree: ITreeNode = {
   label: () => 'Главная',
   route: { name: 'home' },
@@ -32,8 +43,8 @@ const tree: ITreeNode = {
       route: { name: 'catalog' },
       children: [
         {
-          label: () => sections.value?.find((x) => x.id === sectionId.value)?.name,
-          route: { name: 'catalog', params: { sectionId: sectionId.value } },
+          label: () => sections.value?.find((x) => x.id === product.value.catalogSectionId)?.name,
+          route: { name: 'catalog', params: { sectionId: product.value.catalogSectionId } },
           children: [
             {
               label: () => 'Товар',
@@ -56,18 +67,36 @@ const tree: ITreeNode = {
 
 const defaultState: IBreadcrumbStore = {
   list: treeToList(tree),
+  params: {
+    sectionId: null,
+  },
 };
 
 const { getter } = defineStore('breadcrumb', defaultState);
 
 const list = getter('get-list', (state) => state.list);
 
-const getByPathTest = getter('get-test', (state) => {
-  const name = 'catalog';
-  return state.list.find((x) => (x.route as RouteLocation).name === name);
-});
+const breadcrumbItemsByPath = (loc: ILocation) =>
+  getter(`get-test--${loc.name}`, (state) => {
+    const root = state.list.find((x) => x.route.name === loc.name);
+    if (root == null) {
+      throw new Error('Не удалось найти элемент breadcrumb по заданному пути');
+    }
+    const res: IListNode[] = [root];
+    let node: IListNode = root;
+    while (node.parentId != null) {
+      // eslint-disable-next-line no-loop-func
+      const parent = state.list.find((x) => x.id === node.parentId);
+      if (parent != null) {
+        node = parent;
+        res.push(node);
+      }
+    }
+    console.log(res);
+    return res.reverse();
+  });
 
 export const breadcrumbStore = {
   list,
-  getByPathTest,
+  breadcrumbItemsByPath,
 };
--- messages-web/src/vue/base/containers/breadcrumb-container.vue
+++ messages-web/src/vue/base/containers/breadcrumb-container.vue
@@ -4,21 +4,35 @@
 
 <script lang="ts">
 import { breadcrumbStore } from '@/store/breadcrumb.store';
-import { defineComponent } from 'vue';
-import { useRoute } from 'vue-router';
+import { computed, defineComponent } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+interface IBreadcrumbModel {
+  label: string;
+  to: string;
+}
 
 export default defineComponent({
   setup() {
     const route = useRoute();
-    const { list } = breadcrumbStore;
+    const router = useRouter();
+    const { list, breadcrumbItemsByPath } = breadcrumbStore;
+    const breadcrumbs = computed<IBreadcrumbModel[]>(() =>
+      breadcrumbItemsByPath({ name: route.name?.toString() }).value.map(
+        (x) =>
+          ({
+            label: x.label(),
+            to: router.resolve({ name: x.route.name }).fullPath,
+          } as IBreadcrumbModel),
+      ),
+    );
+    const home = computed(() => breadcrumbs.value[0]);
+    const items = computed(() => breadcrumbs.value.slice(1));
     return {
       route,
       list,
-      home: { label: 'Главная', to: '/' },
-      items: [
-        { label: 'Каталог', to: '/catalog' },
-        { label: 'Авиация', to: '/catalog?sectionId=4' },
-      ],
+      home,
+      items,
     };
   },
 });

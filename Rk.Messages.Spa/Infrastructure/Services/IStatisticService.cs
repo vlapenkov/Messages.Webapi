44e73a1880243df9c5bd2d3cd6f4d8240147e196
Врублевский Алексей <Vrublevskiy.AA@roscosmos.digital>
2023-01-30 10:45:07 +0300

    -добавил контроллер в шлюзе
    #4869

---
+++ Rk.Messages.Spa/Infrastructure/Services/IStatisticService.cs
@@ -0,0 +1,21 @@
+﻿using Refit;
+using Rk.Messages.Spa.Infrastructure.Dto.AccountNS;
+using Rk.Messages.Spa.Infrastructure.Dto.StatisticsNS;
+
+namespace Rk.Messages.Spa.Infrastructure.Services;
+
+/// <summary>
+/// Сервис получения статистики
+/// </summary>
+public interface IStatisticService
+{
+    /// <summary>
+    /// Метод получения популятрных(часто просматриваемых) товаров
+    /// </summary>
+    /// <param name="count">колиество</param>
+    /// <param name="from">с</param>
+    /// <param name="to">по</param>
+    /// <returns></returns>
+    [Get("/api/v1/statistics/products/popular")]
+    Task<IReadOnlyCollection<PopularProduct>> TopProducts([Query] int count, [Query] DateTime from, [Query] DateTime to);
+}

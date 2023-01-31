7a2ca1e6c041838b064eeea227a89f0d18ad85c7
Врублевский Алексей <Vrublevskiy.AA@roscosmos.digital>
2023-01-31 16:30:57 +0300

    коментарии

--- Services/Statistic/RK.Statistic.Interfaces/Tables.cs
+++ Services/Statistic/RK.Statistic.Interfaces/Tables.cs
@@ -1,6 +1,12 @@
 ﻿namespace RK.Statistic.Interfaces;
 
+/// <summary>
+/// Названия таблиц
+/// </summary>
 public static class Tables
 {
+    /// <summary>
+    /// Таблица для сохранения просмотров продуктов
+    /// </summary>
     public const string ProductReadTable = "product_read";
 }
